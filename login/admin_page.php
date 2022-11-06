5c15ea10721341c285111cf9221e07eb8be00848
Araz Məmmədov <101507193+mamedov021@users.noreply.github.com>
2022-11-06 22:00:07 +0400

    Add files via upload

---
+++ login/admin_page.php
@@ -0,0 +1,43 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['admin_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>admin page</title>
+   <!-- Bootstrap -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+   <!-- Bootstrap -->
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="/css/style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+   <div class="content">
+      <h3>hi, <span>admin</span></h3>
+      <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
+      <p>this is an admin page</p>
+      <a href="login_form.php" class="btn">login</a>
+      <a href="register_form.php" class="btn">register</a>
+      <a href="logout.php" class="btn">logout</a>
+   </div>
+
+</div>
+
+</body>
+</html>
---
+++ login/user_page.php
@@ -0,0 +1,43 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name'])){
+   header('location:login_form.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>user page</title>
+   <!-- Bootstrap -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+   <!-- Bootstrap -->
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+   <div class="content">
+      <h3>hi, <span>user</span></h3>
+      <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
+      <p>this is an user page</p>
+      <a href="login_form.php" class="btn">login</a>
+      <a href="register_form.php" class="btn">register</a>
+      <a href="logout.php" class="btn">logout</a>
+   </div>
+
+</div>
+
+</body>
+</html>
